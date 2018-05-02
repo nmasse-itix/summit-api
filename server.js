0ff1ec614a2949b7fef64eed0395e16953982b57
Nicolas Massé <nicolas.masse@itix.fr>
2018-05-02 14:12:49 +0200

    v1

--- server.js
+++ server.js
@@ -3,29 +3,40 @@ var app = express();
 var router = express.Router();
 var port = 8080;
 
-router.use(function (req,res,next) {
+router.use(function (req, res, next) {
   next();
   console.log("%s %s => %i", req.method, req.originalUrl, res.statusCode);
 });
 
-router.get("/",function(req,res){
-  res.sendFile(__dirname + '/views/index.html');
-});
-
-router.get("/location",function(req,res){
+router.get("/location", function(req, res) {
   var response = { "Location": "SFO" };
   res.type('application/json')
      .send(JSON.stringify(response))
      .end();
 });
 
+router.get("/timeframe",function(req, res) {
+  var response = { "From": "08/05/2018", "To": "10/05/2018" };
+  res.type('application/json')
+     .send(JSON.stringify(response))
+     .end();
+});
+
+/*
+router.get("/participants",function(req, res) {
+  var response = [ "Mark", "Nicolas" ];
+  res.type('application/json')
+     .send(JSON.stringify(response))
+     .end();
+});
+*/
 
 app.use("/",router);
 
-app.use("*",function(req,res){
+app.use("*",function(req, res) {
   res.status(404).send("Not found");
 });
 
-app.listen(port,function(){
+app.listen(port, function() {
   console.log("Live at Port %i", port);
 });
